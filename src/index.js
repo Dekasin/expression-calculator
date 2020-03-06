75a2d8056597086143a6eb0e3ca6b6e7de383e50
dekasins <dekasins@gmail.com>
2020-03-06 18:49:14 +0300

    feat: simple operation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 function eval() {
     // Do not use eval!!!
-    return;
+    return ;
 }
 
 function expressionCalculator(expr) {
     // write your solution here
+    if (expr.includes("+")) {
+      let  arr=expr.split("+");
+        return arr[0]*1 + arr[1]*1;
+    }
+
+    if (expr.includes("-")) {
+        let  arr=expr.split("-");
+          return (arr[0]*1) - (arr[1]*1);
+      }
+
+    if (expr.includes("*")) {
+    let  arr=expr.split("*");
+        return (arr[0]*1) * (arr[1]*1);
+    }  
+
+    if (expr.includes("/")) {
+    let  arr=expr.split("/");
+       if ( arr[1]*1 != 0 ) {
+            
+        return arr[0]*1 / arr[1]*1 
+       } else {  
+        throw("TypeError: Division by zero.")
+       };
+    }  
+    return 0;
 }
 
 module.exports = {
